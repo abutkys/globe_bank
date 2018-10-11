f0cabbce5ecd8338ef0eaf6ec67655055898637b
abutkys <abutkys4@gmail.com>
2018-10-11 12:15:51 +0300

    test

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -8,6 +8,6 @@
 define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
 define("DB_USER", "bc66d2c7bab7e2");
 define("DB_PASS", "188cd231");
-define("DB_NAME", "globe-bank");
+define("DB_NAME", "globe_bank");
 
 ?>
