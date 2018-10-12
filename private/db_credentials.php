3465195e10ec748088c1150d51a30b2e58543902
abutkys <abutkys4@gmail.com>
2018-10-12 15:08:26 +0300

    test

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,13 +1,13 @@
 <?php
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
-define("DB_NAME", "globe_bank");
-//
-//define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
-//define("DB_USER", "bc66d2c7bab7e2");
-//define("DB_PASS", "188cd231");
-//define("DB_NAME", "heroku_99e04e93b6c6b26");
+//define("DB_SERVER", "localhost");
+//define("DB_USER", "webuser");
+//define("DB_PASS", "secretpassword");
+//define("DB_NAME", "globe_bank");
+
+define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
+define("DB_USER", "bc66d2c7bab7e2");
+define("DB_PASS", "188cd231");
+define("DB_NAME", "heroku_99e04e93b6c6b26");
 
 ?>
